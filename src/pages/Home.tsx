3bf8fd26446c8433540195d77ce1ae4b2133f1da
LucileTech <lucile.t.tech@gmail.com>
2023-03-02 14:09:39 -0800

    redesign home page and add buttons, redesign contact form

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,36 @@
 import React from "react";
 import logo from "./../logowhite.png";
+import avatar from "./../Images/aquarelavatar2.png";
 import { Link, Outlet } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 
 const Home = ({ theme }: { theme: string }) => {
   return (
-    <>
+    <div className="App-homepage">
       <Link className="App-homepage Home-title" to="/works">
         <p>
-          <img className={`LT-logo-${theme}`} src={logo} alt="logo" />
+          <img className="avatar" src={avatar} alt="avatar" />
         </p>
-        <p className={`Home-title-${theme}`}>Lucile Tronczyk</p>
+        {/* <p>
+          <img className={`LT-logo-${theme}`} src={logo} alt="logo" />
+        </p> */}
         <p className={`Home-title-${theme}`}>
-          Full Stack Software Engineer in San Francisco
+          Hi! I am Lucile Tronczyk, Full Stack Web Developer in San Francisco,
+          CA
         </p>
       </Link>
-    </>
+      <div className="App-homepage-buttons">
+        <Link to="/contact">
+          <button className="button-55">Contact me</button>
+        </Link>
+        <Link to="/about">
+          <button className="button-55">Know more about my</button>
+        </Link>
+        <Link to="/works">
+          <button className="button-55">My projects</button>
+        </Link>
+      </div>
+    </div>
   );
 };
 
