2feb00aac74adb112e6d1a9580de9082458c6762
LucileTech <lucile.t.tech@gmail.com>
2023-04-21 16:49:12 +0200

    Loading picture issue

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import avatar from "./../../Images/mebyelfie.webp";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 const Home = ({ theme }: { theme: string }) => {
   return (
     <div className="App-homepage" id="homeSection">
       <img className="avatarbis" src={avatar} alt="avatar" />
+      {/* <LazyLoadImage
+        className="avatarbis"
+        src={avatar}
+        width={600}
+        height={400}
+        alt="Image Alt"
+      /> */}
       <div className={`Home-title ${theme}`}>
         <div className="intro-post-it-now">
           <p className="line-1 anim-typewriter">Hi!</p>
