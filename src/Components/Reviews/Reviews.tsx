ca8304efa07cc99f96a72173100c990af584e64b
LucileTech <lucile.t.tech@gmail.com>
2023-12-08 11:33:18 -0800

    add Jae review

--- src/Components/Reviews/Reviews.tsx
+++ src/Components/Reviews/Reviews.tsx
@@ -3,20 +3,27 @@ import React, { useState } from "react";
 const reviewsData = [
   {
     id: 1,
+    name: "Jae Sabol",
+    position: "Executive Director @One Community Global",
+    review:
+      "I had the pleasure of being her direct supervisor and I'm very happy to recommend her. She is very skilled, professional, detail oriented, a quick learner who takes and integrates feedback well, and is an overall joy to work with. She is a good communicator, works well independently and in a team, and can be depended upon to tackle complex tasks and still hit deadlines. Lucile has a bright future ahead of her and any company that brings her on will be happy they did.",
+  },
+  {
+    id: 2,
     name: "Marie-Esther Rouffet-Degbelo",
     position: "CEO & Co-founder @Koalou",
     review:
       "It was a pleasure working with Lucile. She is listening, and applying accurately our will with lots of motivation. She is doing the extra mile on each of her task. It was a great pleasure working with her! Welcome any time!",
   },
   {
-    id: 2,
+    id: 3,
     name: "Amélie Chabrand",
     position: "Project Manager at @Numberly",
     review:
       "Lucile is passionate, she likes to dive 100% into her projects and is curious in all her approaches. She will always look for solutions and doesn’t get demotivated at the slightest bug: she won’t give up until she has debugged it, she is not a fullstack developer for nothing. Lucile has many assets for teamwork: she has a very good ability to lead and frame a project but she is also very good at collaborating and communicating during critical moments.",
   },
   {
-    id: 3,
+    id: 4,
     name: "Ines Zallouz",
     position: "Ines Zallouz, Full Stack Developer at @Capgemini",
     review:
